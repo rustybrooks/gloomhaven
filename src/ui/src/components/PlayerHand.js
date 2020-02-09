e94074a592a84eb4f09c135121845f2d7942b399
Rusty Brooks <rbrooks@alienvault.com>
2020-02-09 12:56:35 -0600

    Add character card assets and database

--- src/ui/src/components/PlayerHand.js
+++ src/ui/src/components/PlayerHand.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+const character = 'tinkerer'
+import data from '../data/character-ability-cards/tinkerer.json'
+const level = 1
+
 const PlayerHand = () => {
-  return <div>Hand</div>
+  return <div>{
+
+    data.cards.filter(card => card.level === 'X' || parseInt(card.level) <= level).map(card => {
+      const characterString = character.toLowerCase().replace(/ /g, "_");
+      const cardNameString = card.name.toLowerCase().replace(/ /g, "_").replace(/'/g, "");
+
+      return <img key={cardNameString} height={140} src={`/assets/character-ability-cards/${characterString}/${cardNameString}.png`}/>
+    })
+  }</div>
 }
 
 export default PlayerHand
